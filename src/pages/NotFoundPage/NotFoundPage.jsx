2e6f1ac5564dc1dad31655c750b4fbbdba9d19bd
Dula Purkaystha <dpurkays@gmail.com>
2025-03-13 00:20:53 -0700

    Implement NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,20 @@
+import embarassedImage from "../../assets/images/gif/blush.gif";
 import "./NotFoundPage.scss";
 
 function NotFoundPage() {
   return (
     <main className="not-found">
-      <h1 className="not-found__title">Page Not Found</h1>
+      <section className="not-found__context">
+        <h1 className="not-found__title">404 Error</h1>
+        <p className="not-found__text">
+          Well this is embarassing, we can't find this page.
+        </p>
+      </section>
+      <img
+        src={embarassedImage}
+        alt="anime boy blushing"
+        className="not-found__img"
+      />
     </main>
   );
 }
